2122beff04448a366127b89160e2b14d467789a4
James X. Nelson <James@WeTheInter.net>
2017-11-23 07:13:28 -0800

    Improve default method invoker

--- user/src/com/google/gwt/reflect/shared/GwtReflectJre.java
+++ user/src/com/google/gwt/reflect/shared/GwtReflectJre.java
@@ -2,6 +2,7 @@ package com.google.gwt.reflect.shared;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.net.URL;
 
@@ -23,24 +24,35 @@ public final class GwtReflectJre {
         } catch (final ClassNotFoundException ignored) {}
       }
     }
+    // TODO: try using reflection to get access to the constructor we would need to create-a-package.
+    // (perhaps going as far as taking a byte[] template of a generic package,
+    // and just swapping out the name attributes).
     return pkg;
   }
 
   /**
-   * @param method
-   * @param params
-   * @return
-   * @throws Throwable
+   * Do nefarious things to be able to call a default method without an instance.
+   *
+   * This is ONLY safe to do on methods without side effects,
+   * i.e.: {@code default String getMyPrefix() { return "foo" }}
+   * behavior is undefined for attempting to invoke other instance methods inside
+   * default methods that are reflectively invoked with this method.
+   *
+   * Use at your own risk (be sure to test... Without using mocks, preferably).
    */
   public static Object invokeDefaultMethod(final Method method, final Object[] params) throws Throwable {
+    @SuppressWarnings("Convert2Lambda") // we compile to java 1.7
     final Object t=  java.lang.reflect.Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
-        new Class<?>[]{method.getDeclaringClass()},new java.lang.reflect.InvocationHandler() {
-      @Override
-      public Object invoke(final Object proxy, final Method method, final Object[] args)
-          throws Throwable {
-        return null;
-      }
-    });
+        new Class<?>[]{method.getDeclaringClass()}, new InvocationHandler() {
+          @Override
+          public Object invoke(Object proxy, Method foreign, Object[] args) throws Throwable {
+            // TODO: actually handle chained calls
+            assert method.getName().equals(foreign.getName()) : "Calling unhandled method named " + foreign.toGenericString();
+            assert method.toGenericString().equals(foreign.toGenericString()) : "Calling unhandled method named " + foreign.toGenericString();
+            return null;
+          }
+        }
+    );
 
     final Field field = java.lang.invoke.MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
     field.setAccessible(true);
